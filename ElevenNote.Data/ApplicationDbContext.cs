8846795d0002626f27da326fb9b34f06fd7b5cf6
jamiecoakley <jamiemcoakley@gmail.com>
2022-12-14 20:00:33 -0500

    After spending far too much time on this, 4.01 is done.

---
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElevenNote.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
+
+//base constructor gets called first. Can take in those options.Options of type DbContext options takes in a generic type, in our case Application DbContext.
+//Options trickle down to the parent constructor options
+//This allows us to make options. Once we make those, they're initialized from the parent object
--- ElevenNote.WebAPI/Program.cs
+++ ElevenNote.WebAPI/Program.cs
@@ -1,4 +1,8 @@
+using ElevenNote.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+//<<this trickles down to parent object's constructor, then use sqlserver
+{
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
