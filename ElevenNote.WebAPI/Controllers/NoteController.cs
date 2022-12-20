2cc31886337a6b31bc9c9b35e82ef9b5f59dac51
jamiecoakley <jamiemcoakley@gmail.com>
2022-12-19 19:06:07 -0500

    Added NoteController with authorization

---
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using ElevenNote.Services.Note;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+        public NoteController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+    }
+}
